ff371ea983109ed38ea83e26ee752f5ec12f815d
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-29 11:52:01 +0100

    Update install-bin-main-Ubuntu-12.04.sh

--- install/install-bin-main-Ubuntu-12.04.sh
+++ install/install-bin-main-Ubuntu-12.04.sh
@@ -138,12 +138,12 @@ rm -rf /tmp/update/XtreamUI-master >/dev/null
 rm /tmp/update.zip >/dev/null
 rm -rf /tmp/update >/dev/null
 wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/nginx.conf.final" -O /home/xtreamcodes/iptv_xtream_codes/nginx/conf/nginx.conf
-wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/wwwdir/darkino.php" -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php
-chattr +i /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php
-wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/episode.php" -O /home/xtreamcodes/iptv_xtream_codes/admin/episode.php
-chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/episode.php
-wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/movie.php" -O /home/xtreamcodes/iptv_xtream_codes/admin/movie.php
-chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/movie.php
+wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/wwwdir/darkino.php" -qO /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null
+chattr +i /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null
+wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/episode.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null
+chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null
+wget "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/movie.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/movie.php >/dev/null
+chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/movie.php >/dev/null
 chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/ >/dev/null
 chmod +x /home/xtreamcodes/iptv_xtream_codes/permissions.sh >/dev/null
 /home/xtreamcodes/iptv_xtream_codes/permissions.sh >/dev/null
