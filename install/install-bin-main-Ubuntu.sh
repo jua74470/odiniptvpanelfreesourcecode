d72cde2e63c4ef00a2a699b2189b19034885fae3
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-30 02:14:09 +0100

    Update install-bin-main-Ubuntu.sh

--- install/install-bin-main-Ubuntu.sh
+++ install/install-bin-main-Ubuntu.sh
@@ -627,18 +627,15 @@ chmod -R 0777 /home/xtreamcodes
 mkdir -p /etc/mysql/
 wget --no-check-certificate https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/my.cnf -O /etc/mysql/my.cnf
 wget --no-check-certificate https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/my.cnf -O /etc/my.cnf
-mv /etc/init.d/mysql /etc/init.d/mariadb
-chmod +x /etc/init.d/mariadb
-service mariadb restart
 wget --no-check-certificate -q -O "/tmp/xtreamcodes.tar.xz" "https://github.com/jua74470/odiniptvpanelfreesourcecode/releases/download/download/main_xtreamcodes_reborn_nobin.tar.xz"
 mkdir -p /home/xtreamcodes/
-tar -xvf "/tmp/xtreamcodes.tar.xz" -C "/home/xtreamcodes/"
-chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb >/dev/null
+tar -xf "/tmp/xtreamcodes.tar.xz" -C "/home/xtreamcodes/"
+chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb >/dev/null 2>&1
 rm -rf /home/xtreamcodes/iptv_xtream_codes/admin 2>/dev/null
 cd /home/xtreamcodes
 rm -rf phpbuild
 wget --no-check-certificate https://github.com/jua74470/odiniptvpanelfreesourcecode/releases/download/download/phpbuild-php7.2-Ubuntu-$LTS.tar.xz
-tar -xvf phpbuild-php7.2-Ubuntu-$LTS.tar.xz
+tar -xf phpbuild-php7.2-Ubuntu-$LTS.tar.xz
 rm -f phpbuild-php7.2-Ubuntu-$LTS.tar.xz
 cd /home/xtreamcodes/phpbuild/nginx_nginx-1.24.0
 mkdir -p "/home/xtreamcodes/iptv_xtream_codes/nginx/"
@@ -653,7 +650,7 @@ kill $(ps aux | grep 'xtreamcodes' | grep -v grep | grep -v 'start_services.sh' 
 killall nginx
 kill $(ps aux | grep 'xtreamcodes' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
 rm -f /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/*
-make install
+make install >/dev/null 2>&1
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx/conf/balance.conf https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/balance.conf
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx/conf/fastcgi.conf https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/fastcgi.conf
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx/conf/fastcgi.conf.default https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/fastcgi.conf.default
@@ -688,7 +685,7 @@ kill $(ps aux | grep 'xtreamcodes' | grep -v grep | grep -v 'start_services.sh' 
 rm -f /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/*
 #mv objs/nginx objs/nginx_rtmp
 #cp objs/nginx_rtmp /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/
-make install
+make install >/dev/null 2>&1
 mv /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/balance.conf https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/balance.conf
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/fastcgi.conf https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/fastcgi.conf
@@ -709,40 +706,41 @@ wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/co
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/uwsgi_params.default https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/uwsgi_params.default
 wget --no-check-certificate -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/win-utf https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/win-utf
 cd /home/xtreamcodes/phpbuild/php-7.2.34
-make install
+make install >/dev/null 2>&1
 cd /home/xtreamcodes/phpbuild/mcrypt-1.0.5
-make install
+make install >/dev/null 2>&1
 cd /home/xtreamcodes/phpbuild/geoip-1.1.1
-make install
+make install >/dev/null 2>&1
 cd /home/xtreamcodes/phpbuild/igbinary-3.2.14
-make install
+make install >/dev/null 2>&1
 cd /home/xtreamcodes/phpbuild/
-wget --no-check-certificate https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-tar -xvf ioncube_loaders_lin_x86-64.tar.gz
+wget --no-check-certificate -qO /home/xtreamcodes/phpbuild/ioncube_loaders_lin_x86-64.tar.gz https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar -xf /home/xtreamcodes/phpbuild/ioncube_loaders_lin_x86-64.tar.gz >/dev/null 2>&1
 cp ioncube/ioncube_loader_lin_7.2.so /home/xtreamcodes/iptv_xtream_codes/php/lib/php/extensions/no-debug-non-zts-20170718/
-wget --no-check-certificate -O /tmp/update.zip "https://github.com/jua74470/odiniptvpanelfreesourcecode/releases/download/download/update_original.zip" 2>/dev/null
-unzip /tmp/update.zip -d /tmp/update/ >/dev/null
-cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/ >/dev/null
-rm -rf /tmp/update/XtreamUI-master >/dev/null
-rm /tmp/update.zip >/dev/null
-rm -rf /tmp/update >/dev/null
-rm -f /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg
-rm -f /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe
-ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg_2018 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg
-ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe_2018 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe
-chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg
-chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe
-chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg_2018
-chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe_2018
-wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/nginx.conf.final" -O /home/xtreamcodes/iptv_xtream_codes/nginx/conf/nginx.conf
-wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/wwwdir/darkino.php" -qO /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null
-chattr +i /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null
-wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/episode.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null
-chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null
-wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/movie.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/movie.php >/dev/null
-chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/movie.php >/dev/null
-chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/  2>/dev/null
-chmod +x /home/xtreamcodes/iptv_xtream_codes/permissions.sh >/dev/null
-/home/xtreamcodes/iptv_xtream_codes/permissions.sh  2>/dev/null
-find /home/xtreamcodes/ -type d -not \( -name .update -prune \) -exec chmod -R 777 {} + >/dev/null
+wget --no-check-certificate -qO /tmp/update.zip "https://github.com/jua74470/odiniptvpanelfreesourcecode/releases/download/download/update_original.zip" >/dev/null 2>&1
+unzip /tmp/update.zip -d /tmp/update/ >/dev/null 2>&1
+cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/ >/dev/null 2>&1
+rm -rf /tmp/update/XtreamUI-master >/dev/null 2>&1
+rm /tmp/update.zip >/dev/null 2>&1
+rm -rf /tmp/update >/dev/null 2>&1
+rm -f /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg >/dev/null 2>&1
+rm -f /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe >/dev/null 2>&1
+ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg_2018 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg >/dev/null 2>&1
+ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe_2018 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe >/dev/null 2>&1
+chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg >/dev/null 2>&1
+chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe >/dev/null 2>&1
+chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg_2018 >/dev/null 2>&1
+chmod 777 /home/xtreamcodes/iptv_xtream_codes/bin/ffprobe_2018 >/dev/null 2>&1
+wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/nginx/conf/nginx.conf.final" -qO /home/xtreamcodes/iptv_xtream_codes/nginx/conf/nginx.conf >/dev/null 2>&1
+wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/wwwdir/darkino.php" -qO /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null 2>&1
+chattr +i /home/xtreamcodes/iptv_xtream_codes/wwwdir/darkino.php >/dev/null 2>&1
+wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/episode.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null 2>&1
+chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/episode.php >/dev/null 2>&1
+wget --no-check-certificate "https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/master/admin/movie.php" -qO /home/xtreamcodes/iptv_xtream_codes/admin/movie.php  >/dev/null 2>&1
+chattr +i /home/xtreamcodes/iptv_xtream_codes/admin/movie.php >/dev/null 2>&1
+chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/ >/dev/null 2>&1
+chmod +x /home/xtreamcodes/iptv_xtream_codes/permissions.sh >/dev/null 2>&1
+chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb >/dev/null 2>&1
+/home/xtreamcodes/iptv_xtream_codes/permissions.sh  >/dev/null 2>&1
+find /home/xtreamcodes/ -type d -not \( -name .update -prune \) -exec chmod -R 777 {} +  >/dev/null 2>&1
 
