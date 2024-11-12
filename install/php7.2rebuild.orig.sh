a94bbe605feb525f206470b7de7f5692b68d1fed
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-11-12 19:42:34 +0100

    Update php7.2rebuild.orig.sh

--- install/php7.2rebuild.orig.sh
+++ install/php7.2rebuild.orig.sh
@@ -2,7 +2,8 @@
 # sudo apt-get update
 # sudo apt-get -y install curl
 # sudo yum -y install curl
-# sudo curl -L --output /root/php7.2rebuild.orig.sh https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/installer/install/php7.2rebuild.orig.sh; bash /root/php7.2rebuild.orig.sh
+# sudo curl -L --output /root/depbuild.sh https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/master/install/depbuild.sh;fi; sudo bash /root/depbuild.sh
+# sudo curl -L --output /root/php7.2rebuild.orig.sh https://github.com/jua74470/odiniptvpanelfreesourcecode/raw/refs/heads/installer/install/php7.2rebuild.orig.sh; sudo bash /root/php7.2rebuild.orig.sh
 echo -e "\nChecking that minimal requirements are ok"
 # Ensure the OS is compatible with the launcher
 if [ -f /etc/almalinux-release ]; then
